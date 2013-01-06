8379e4f4fb7a1c9e7de7cf1420555acd6992b829
Jason Hazel <me@jasonhazel.com>
2013-01-05 23:02:45 -0500

    reworked.

--- eggtt.js
+++ eggtt.js
@@ -1,156 +1,109 @@
 /**
  * This used to be Izzmo's Auto Awesome (https://github.com/izzmo/AutoAwesomer)
- * I stripped pretty much everything out.  I want it for the core bits.
+ * I stripped almost everything out, and what remained was reworked.
+ *
+ * javascript:(function(){$('body').append('<script src=\'https://github.com/mrhazel/eggtt/blob/master/eggtt.js\'></script>');})();
  */
 
-$(document).ready(function() {
-  window.eggtt = {
-    ttObj: null,
-    init: function() {
-      $('.roomView').ready(function() {
-        window.eggtt.ttObj = window.turntable.buddyList.room;
-        if(window.eggtt.ttObj === null) {
-          alert('Could not find turntable.fm objects. You should refresh your page and try again.');
-          return;
-        }
-        window.eggtt.menu.build();
-        turntable.addEventListener("message", window.eggtt.listener);
-      });
-    },
-    vote: function(c) {
-      console.debug(window.eggtt.ttObj);
-      var f = $.sha1(window.eggtt.ttObj.roomId + c + window.eggtt.ttObj.currentSong._id);
-      var d = $.sha1(Math.random() + "");
-      var e = $.sha1(Math.random() + "");
-      this.socket({
-          api: "room.vote",
-          roomid: this.ttObj.roomId,
-          section: this.ttObj.section,
-          val: c,
-          vh: f,
-          th: d,
-          ph: e
-      });
-    },
-    socket: function (c, a) {
-        if (c.api == "room.now") return;
 
-        c.msgid = turntable.messageId;
-        turntable.messageId += 1;
-        c.clientid = turntable.clientId;
-        if (turntable.user.id && !c.userid) {
-            c.userid = turntable.user.id;
-            c.userauth = turntable.user.auth;
+$(document).ready(function() {
+
+  eggtt = { 
+    init: function() {
+
+      this.api.vote('up');
+
+      this.autoqueue.init();
+
+      //inject css
+      // var head = $('head');
+      // 
+      // 
+      
+      $('head').append($("<link rel='stylesheet' href='https://raw.github.com/mrhazel/eggtt/master/eggtt.css' type='text/css'>"));
+
+      turntable.addEventListener("message", this.autoqueue.listener);
+    },
+    menu : {
+      $main: null,
+      init : function() {
+        this.$main = $('ul#settings-dropdown');
+      },
+      add: function(id, klass, title) {
+        if (this.$main === null) this.init();
+
+        var $new_menu = $("<li>")
+                          .addClass('option' + (klass ? ' '+klass : '')) // only because the extra space would drive me mad.
+                          .attr('id', 'eggtt-' + id)
+                          .html(title);
+
+        $new_menu.prependTo(this.$main);
+        return $new_menu;
+      }
+    },
+    autoqueue : {
+      active: false,
+      listener: function(data) {
+        console.debug(data);
+      },
+      init: function() {
+        this.$autoqueue = eggtt.menu.add('autoqueue','menu-off','Auto Queue');
+        this.$autoqueue.click(this.toggle);
+      },
+      toggle: function() {
+        alert(1);
+      }
+    },
+    api : {
+      send: function(command, handler) {
+        if (command.api == "room.now") return;
+
+        command.msgid = turntable.messageId++;
+        // turntable.messageId += 1;
+        command.clientid = turntable.clientId;
+        if (turntable.user.id && !command.userid) {
+            command.userid = turntable.user.id;
+            command.userauth = turntable.user.auth;
         }
-        var d = JSON.stringify(c);
+        var json = JSON.stringify(command);
         if (turntable.socketVerbose) {
-            LOG(util.nowStr() + " Preparing message " + d);
+            LOG(util.nowStr() + " Preparing message " + json);
         }
-        var b = $.Deferred();
+        var deferred = $.Deferred();
         turntable.whenSocketConnected(function () {
             if (turntable.socketVerbose) {
-                LOG(util.nowStr() + " Sending message " + c.msgid + " to " + turntable.socket.host);
+                LOG(util.nowStr() + " Sending message " + command.msgid + " to " + turntable.socket.host);
             }
             if (turntable.socket.transport.type == "websocket") {
-                turntable.socketLog(turntable.socket.transport.sockets[0].id + ":<" + c.msgid);
+                turntable.socketLog(turntable.socket.transport.sockets[0].id + ":<" + command.msgid);
             }
-            turntable.socket.send(d);
+            turntable.socket.send(json);
             turntable.socketKeepAlive(true);
             turntable.pendingCalls.push({
-                msgid: c.msgid,
-                handler: a,
-                deferred: b,
+                msgid: command.msgid,
+                handler: handler,
+                deferred: deferred,
                 time: util.now()
             });
         });
-        return b.promise();
-    },
-    listener : function(d) {
-      if (d.command) {
-        switch(d.command) {
-          case 'newsong':
-            setTimeout(window.eggtt.awesome(), 15000);
-            break;
-        }
-      }
-    },
-    add_dj  : function() { this.socket({api: 'room.add_dj', roomid: this.ttObj.roomId }); },
-    awesome : function() { this.vote('up');     },
-    lame    : function() { this.vote('down');   },
-    destroy : function() { this.menu.destroy(); },
-    menu : {
-      autodj : {
-        build : function() {
-          this.$main = $("<li class='option' id='eggtt-autodj'>Auto DJ</li>");
-          this.$main.prependTo($('ul#settings-dropdown'));
+        return deferred.promise();
+      }, //end send
+      vote: function(c) {
+        console.debug(turntable.buddyList.room);
+        var f = $.sha1(turntable.buddyList.room.roomId + c + turntable.buddyList.room.currentSong._id);
+        var d = $.sha1(Math.random() + "");
+        var e = $.sha1(Math.random() + "");
+        this.send({api: "room.vote", roomid: turntable.buddyList.room.roomId, section: turntable.buddyList.room.section, val: c, vh: f, th: d, ph: e });
+      }, //end vote
+      up: function() {
+        this.vote('up');
+      }, //end up
+      down: function() {
+        this.vote('down');
+      } //end down
+    }//end api
+  }//end eggtt
 
-          this.$status = $("<li style='text-align:right;' class='option eggtt-autodj id='eggtt-autodj-status'>OFF</li>").hide();
-          this.$main.data('status','OFF');
-          this.$main.after(this.$status);
-          
-          this.$type = $("<li class='option eggtt-autodj id='eggtt-autodj-type'>One Shot</li>").hide();
-          this.$main.data('type','one_shot');
-          this.$status.after(this.$type);
-
-
-          this.$delay = $("<li class='option eggtt-autodj id='eggtt-autodj-delay'>5 sec</li>").hide();
-          this.$main.data('delay',5);
-          this.$type.after(this.$delay);
-
-          this.$main.click(function(){
-            $('li.eggtt-autodj').slideToggle();
-          });
-
-          this.$status.click(function(){
-            var main = window.eggtt.menu.autodj.$main;
-            // alert(main.html());
-            switch (main.data('status')) 
-            {
-              case 'OFF':
-                  main.data('status','ON');
-                break;
-              case 'ON':
-                  main.data('status','OFF');
-                break;
-            }            
-
-            window.eggtt.menu.autodj.$status.html(main.data('status'));
-
-
-          });
-        } 
-      },
-      build: function() {
-        // this.$main = $("<li class='option' id='eggtt'>EggTT</li>");
-        // this.$main.prependTo($('ul#settings-dropdown'));
-        
-        // this.$crowd = $("<li class='option eggtt-submenu id='eggtt-crowd'>Toggle Crowd</li>").hide();
-        // this.$main.after(this.$crowd);
-
-        this.autodj.build();
-        // //-- register click events
-        // this.$main.click(this.main);
-        // this.$crowd.click(this.crowd);
-      },
-      main : function() {
-        //-- REMEMBER: when in click events, use the full variable (window.eggtt.BLAH)
-        // alert('I made a menu!');
-        $('li.eggtt-submenu').slideToggle();
-      },
-      crowd : function() {
-        var audience = $('div#audience').toggle();
-      },
-      about : function() {
-        alert('THIS IS AN ALERT BOX!  WHY? BECAUSE I\'M AN ADULT AND I\'LL YELL IF I WANT TO!');
-      },
-      destroy : function() {
-        this.$main.remove();
-        this.$crowd.remove();
-      }
-
-
-    }
-  }
-  
-  window.eggtt.init();
+  eggtt.init();
+  // app = new eggtt
 });
